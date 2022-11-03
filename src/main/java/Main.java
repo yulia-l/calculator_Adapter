654ce3d6ce7c27af4beb0098221ca67edd45bca0
yulia-l <yulia.lyugunaya@yandex.ru>
2022-11-03 13:59:30 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(2, 2));
+        System.out.println(calc.mult(10, 22));
+        System.out.println(calc.pow(2, 10));
+    }
+}
